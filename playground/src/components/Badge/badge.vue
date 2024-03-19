1afdaaa28b16fe9c89ec07631396529abb25e83c
wangliangxin3 <wangliangxin3@jd.com>
2024-03-19 14:16:31 +0800

    fix: 更新

--- playground/src/components/Badge/badge.vue
+++ playground/src/components/Badge/badge.vue
@@ -3,8 +3,14 @@
 <template>
   <div class="will-badge">
     <slot></slot>
+    <Transition name="will-zoo-in-center">
+      <sup
+        v-show="!hidden && (content || isDot)"
+        :class="['will-badge__content']"
+        v-text="content"
+      >
+      </sup>
+    </Transition>
   </div>
 </template>
-<style scoped>
-
-</style>
+<style scoped></style>
