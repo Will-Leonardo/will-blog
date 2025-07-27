ec9ec60b36e5a1b5418ea3eb9c7e777f9a15c2fd
wangliangxin3 <wangliangxin3@jd.com>
2025-07-27 17:53:06 +0800

    fix: 调整部署仓库地址信息

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 # 现已通过GitHub Actions自动部署，提交只需 'git push' 即可
+###
+ # @Autor: wangliangxin3
+ # @Email: wangliangxin3@jd.com
+ # @Description: 
+ # @Date: 2024-12-17 17:05:56
+ # @LastEditors: wangliangxin3
+ # @LastEditTime: 2025-07-27 17:52:55
+### 
 # 若不想使用自动部署，可以直接运行命令 'npm run deploy' 本地自动提交部署
 
 # 确保脚本抛出遇到的错误
@@ -21,7 +29,7 @@ git add -A
 git commit -m 'deploy'
 # git remote add origin https://gitee.com/willwong/will.git
 
-git remote add origin https://github.com/wangliangxin/will-blog.git
+git remote add origin https://github.com/Will-Leonardo/will-blog.git
 
 #git checkout -b gh-pages
 
